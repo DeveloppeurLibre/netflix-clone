6a6be15b5766f8510a2c08e23801b0c76f0a756e
Quentin Cornu <contact@developpeurlibre.com>
2024-05-21 16:44:07 +0200

    Créer un modèle de série

---
+++ NetflixClone/Media.swift
@@ -0,0 +1,57 @@
+//
+// Created for NetflixClone
+//
+
+import Foundation
+
+struct Media {
+    let title: String
+    let coverURL: URL?
+    let isNetflixProduction: Bool
+    let hasNewEpisodes: Bool
+    let topRated: Int?
+    
+    init(
+        title: String,
+        coverURL: URL?,
+        isNetflixProduction: Bool = false,
+        hasNewEpisodes: Bool = false,
+        topRated: Int? = nil
+    ) {
+        self.title = title
+        self.coverURL = coverURL
+        self.isNetflixProduction = isNetflixProduction
+        self.hasNewEpisodes = hasNewEpisodes
+        self.topRated = topRated
+    }
+}
+
+let previewMediums = [
+    Media(
+        title: "Friends",
+        coverURL: URL(string: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2koX1xLkpTQM4IZebYvKysFW1Nh.jpg")
+    ),
+    Media(
+        title: "Outer Banks ",
+        coverURL: URL(string: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ovDgO2LPfwdVRfvScAqo9aMiIW.jpg"),
+        isNetflixProduction: true,
+        hasNewEpisodes: true,
+        topRated: 10
+    ),
+    Media(
+        title: "Fiasco",
+        coverURL: URL(string: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fWMCScVRF1YJ0ofNl3CqZy9Z2hM.jpg"),
+        isNetflixProduction: true,
+        hasNewEpisodes: true,
+        topRated: 9
+    ),
+    Media(
+        title: "The Circle",
+        coverURL: URL(
+            string: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/bQRxhfXXsH4ccScOBHs51bgwNe4.jpg"
+        ),
+        isNetflixProduction: true,
+        hasNewEpisodes: true,
+        topRated: nil
+    )
+]
---
+++ NetflixClone/MediaCell.swift
@@ -0,0 +1,79 @@
+//
+// Created for NetflixClone
+//
+
+import SwiftUI
+
+struct MediaCell: View {
+    
+    let media: Media
+    
+    var body: some View {
+        AsyncImage(url: media.coverURL) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        } placeholder: {
+            Image("placeholder")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        }
+        .frame(width: 110, height: 150)
+        .clipped()
+        
+        // MARK: Netflix Logo
+        .overlay(alignment: .topLeading) {
+            if media.isNetflixProduction {
+                Image("Netflix_logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 16, height: 16)
+                    .padding(4)
+            }
+        }
+        
+        // MARK: New Episodes
+        .overlay(alignment: .bottom) {
+            if media.hasNewEpisodes {
+                Text("New Episodes")
+                    .font(.system(size: 8, weight: .semibold))
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 4)
+                    .padding(.vertical, 2)
+                    .background(Color(red: 0.82, green: 0.184, blue: 0.145))
+            }
+        }
+        
+        // MARK: Top Rating
+        .overlay(alignment: .topTrailing) {
+            if media.topRated != nil {
+                ZStack {
+                    Rectangle()
+                        .foregroundStyle(Color(red: 0.82, green: 0.184, blue: 0.145))
+                        .frame(width: 15, height: 22)
+                        .shadow(color: .black.opacity(0.3), radius: 1)
+                        .rotation3DEffect(
+                            .degrees(-12),
+                            axis: (x: 0.0, y: 1.0, z: 0.0)
+                        )
+                        .offset(x: 0, y: -4)
+                    VStack(spacing: -2) {
+                        Text("TOP")
+                            .font(.system(size: 5))
+                        Text("10")
+                            .font(.system(size: 8))
+                    }
+                    .offset(x: 0, y: -2)
+                    .fontWeight(.heavy)
+                    .foregroundStyle(Color.white)
+                }
+            }
+        }
+        
+        .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    MediaCell(media: previewMediums[2])
+}
