3aeb5037362a70c28eb7b0012da2fc3f65a16cae
Quentin Cornu <contact@developpeurlibre.com>
2024-05-21 18:10:24 +0200

    Barre de navigation personnalisée

---
+++ NetflixClone/HomeView.swift
@@ -0,0 +1,47 @@
+//
+// Created for NetflixClone
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                CategorySlider(title: "Ensemble TV Comedies", mediums: previewMediums)
+                CategorySlider(title: "US TV Programs", mediums: previewMediums)
+            }
+            .padding(8)
+            .padding(.top, 50)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .scrollIndicators(.hidden)
+        .overlay(alignment: .top) {
+            Rectangle()
+                .frame(height: 100)
+                .background(Material.ultraThinMaterial)
+                .overlay(alignment: .bottom) {
+                    ZStack {
+                        HStack {
+                            Text("For Quentin")
+                                .font(.system(size: 24, weight: .bold))
+                            Spacer()
+                            Image(systemName: "airplayvideo")
+                                .font(.system(size: 20, weight: .medium))
+                            Image(systemName: "magnifyingglass")
+                                .font(.system(size: 20, weight: .medium))
+                        }
+                        .foregroundStyle(Color.white)
+                        .padding(20)
+                        .background(Color.black.opacity(0.3))
+                    }
+                }
+                .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
