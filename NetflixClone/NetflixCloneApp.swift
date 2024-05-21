bdba4b6cf399aeb95ae615549d8ab5480d6c47e2
Quentin Cornu <contact@developpeurlibre.com>
2024-05-21 15:30:55 +0200

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,14 @@
+//
+// Created for NetflixClone
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
